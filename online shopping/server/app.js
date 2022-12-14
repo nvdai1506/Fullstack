583099be373b6076fccff62727eef71eb778115a
nvdai1506 <83803775+nvdai1506@users.noreply.github.com>
2022-12-14 11:30:37 +0700

    update cors

--- online shopping/server/app.js
+++ online shopping/server/app.js
@@ -1,15 +1,15 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express';
 import morgan from 'morgan';//logger
 import helmet from 'helmet';//for header security
 import mongoose from 'mongoose';
 import cors from "cors";
-import { v4 } from'uuid';
-import  multer from'multer';//serve for files
+import { v4 } from 'uuid';
+import multer from 'multer';//serve for files
 
 import AdminRouter from './routes/admin.route.js';
 import AuthRouter from './routes/auth.route.js';
@@ -19,25 +19,49 @@ import ShopRouter from './routes/shop.route.js';
 
 import Auth from './middlewares/auth.mdw.js';
 
-const app= express();
+const app = express();
 
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
+// let whitelist = ["http://localhost:3000"];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       console.warn(`Origin '${origin}' is not in the white list:`, whitelist);
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
 app.use(helmet());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
 const fileStorage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, 'images');
-},
-filename: function(req, file, cb) {
-    cb(null, v4()+'-'+ file.originalname)
-}
+  },
+  filename: function (req, file, cb) {
+    cb(null, v4() + '-' + file.originalname)
+  }
 });
 const fileFilter = (req, file, cb) => {
   if (
@@ -59,8 +83,8 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/', AuthRouter);
 app.use('/shop', ShopRouter);
-app.use('/admin',Auth, AdminRouter);
-app.use('/user', Auth , UserRouter);
+app.use('/admin', Auth, AdminRouter);
+app.use('/user', Auth, UserRouter);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -69,11 +93,13 @@ app.use((error, req, res, next) => {
   const data = error.data;
   res.status(status).json({ message: message, data: data });
 });
-
+app.get('/hello', (req, res, next) => {
+  res.json({ mess: "hello from nodejs" });
+})
 
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect(process.env.MONGODB_URI).then( result =>{
+mongoose.connect(process.env.MONGODB_URI).then(result => {
   app.listen(PORT, function () {
     console.log(`Server is listening at http://localhost:${PORT}`);
   });
--- online shopping/server/test.js
+++
@@ -1,6 +0,0 @@
-import moment from 'moment';
-
-// console.log(moment.locale(''));
-console.log(moment().format('LTS'))
-const date = moment().format();
-console.log(date);
